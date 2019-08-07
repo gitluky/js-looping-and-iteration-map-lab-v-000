babe7100d2e4a93774a529604e00f1e63d83eb6b
Jonathan Luk <jluk87@gmail.com>
2019-08-07 18:15:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,3 +5,23 @@ function lowerCaseDrivers (drivers) {
   });
   return lowerCasedDrivers;
 }
+
+function nameToAttributes (drivers) {
+  const driversObject = []
+  let firstName = ''
+  let lastName = ''
+  
+  drivers.map( function (driver) {
+    
+    let i = 0;  
+    while (i < driver.length){
+      if (driver[i] === " ") {
+        break;
+      }
+      i++;
+    }
+    
+    return Object.assign({}, { firstName: driver.slice(0,i) }, { lastName: driver.slice(i+1) });
+
+  })
+}
